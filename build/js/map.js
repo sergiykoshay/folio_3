dee8e2e494ffd27273c1b42ed2e92b662b5453b5
Sergiy <sergiy.koshay@gmail.comgit config --global user.email sergiy.koshay@gmail.comgit config --global user.email sergiy.koshay@gmail.com>
2018-02-07 20:26:25 +0200

    Some bug fix

--- build/js/map.js
+++ build/js/map.js
@@ -1,3 +1,5 @@
+
+
 function initMap() {
     var antalia = {lat: 36.75649, lng: 30.80566};
     var cyprus = {lat: 34.72355, lng: 33.20068};
@@ -5,6 +7,8 @@ function initMap() {
       zoom: 4,
       center: antalia,
     });
+    var geocoder = new google.maps.Geocoder;
+    var infowindow = new google.maps.InfoWindow;
     
     
 
@@ -14,7 +18,7 @@ function initMap() {
     });
 
 
-google.maps.event.addListener(map, 'click', function(event) {
+  google.maps.event.addListener(map, 'click', function(event) {
 
     if (marker != undefined) {
       marker.setMap(null);
@@ -34,6 +38,7 @@ google.maps.event.addListener(map, 'click', function(event) {
         animation: google.maps.Animation.DROP,
         visible: true
     });
+    
     $.ajax({
         type: "POST",
         url: "http://api.openweathermap.org/data/2.5/weather?lat=" + latLat + "&lon=" + latLng +"&appid=d735182480adde5eb03094b44670d2fe",
@@ -43,14 +48,40 @@ google.maps.event.addListener(map, 'click', function(event) {
             var weather = lat.main;
             var temp = weather.temp;//kelvin
             var cels = ~~(temp - 273.15);
-
-            document.getElementById("table").innerHTML = cels;
-            console.log(cels);
-             
+            var wind = lat.wind;
+            var windSpeed = wind.speed;
+            var windDirection = ~~(wind.deg);
+            var windIcon = "towards-" + windDirection + "-deg";
+            document.getElementById("temp").innerHTML = cels;
+            
+            document.getElementById("wind").innerHTML = wind.speed + "m/s ";
+            document.getElementById("winwDirection").classList.add("fa", "fa-arrow-up");
+            document.getElementById("winwDirection").setAttribute("style", "transform: rotate("+windDirection+"deg)")
+            console.log(cels);    
         }
-});
+    });
    
 
+  });
+}
+
+function geocodeLatLng(geocoder, map, infowindow, latlng) {
+  geocoder.geocode({'location': latlng}, function(results, status) {
+    if (status === 'OK') {
+      if (results[1]) {
+        map.setZoom(11);
+        var marker = new google.maps.Marker({
+          position: latlng,
+          map: map
+        });
+        infowindow.setContent(results[1].formatted_address);
+        infowindow.open(map, marker);
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
   });
 }
 
